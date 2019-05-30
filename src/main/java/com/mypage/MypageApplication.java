e079aae22bf38ecd16c5b3b4ec0d455a54a5a3ae
孟少昆 <shaokun.meng@jimu.com>
2019-05-30 16:57:55 +0800

    +war包设置

--- src/main/java/com/mypage/MypageApplication.java
+++ src/main/java/com/mypage/MypageApplication.java
@@ -2,12 +2,18 @@ package com.mypage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MypageApplication {
+public class MypageApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MypageApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MypageApplication.class);
+	}
 }
