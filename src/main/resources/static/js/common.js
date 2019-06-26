21e89641378f6f7ef42ac15975a75e58c040f365
孟少昆 <shaokun.meng@jimu.com>
2019-06-26 19:40:32 +0800

    +登录页面

---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,22 @@
+/*
+    通用css文件
+*/
+/*设置页面icon*/
+document.write('<link rel="shortcut icon" href="../img/mypage_title_icon.png" type="text/css">');
+/*
+/!*新 Bootstrap4 核心 CSS 文件*!/
+document.write('<link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/4.1.0/css/bootstrap.min.css" type="text/css">');
+
+
+/!*
+    通用js文件
+*!/
+/!*jQuery文件。务必在bootstrap.min.js 之前引入*!/
+document.write('<script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js" type="text/javascript"></script>');
+/!*popper.min.js 用于弹窗、提示、下拉菜单*!/
+document.write('<script src="https://cdn.staticfile.org/popper.js/1.12.5/umd/popper.min.js" type="text/javascript"></script>');
+/!*最新的 Bootstrap4 核心 JavaScript 文件*!/
+document.write('<script src="https://cdn.staticfile.org/twitter-bootstrap/4.1.0/js/bootstrap.min.js" type="text/javascript"></script>');
+/!*jquery表单校验插件*!/
+document.write('<script src="https://cdn.staticfile.org/jquery-validate/1.19.1/jquery.validate.min.js" type="text/javascript"></script>');
+*/
